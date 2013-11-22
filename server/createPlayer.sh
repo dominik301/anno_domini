efad06e090ed92fc8830219e12c355050e8b0ffc
rds89 <rds1989@hotmail.it>
2013-11-22 09:55:32 +0100

    Aggiornati script per curl con controllo dell'input

--- server/createPlayer.sh
+++ server/createPlayer.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+if test $# -ne 1; then  echo "Uso: $0 <username>";  exit 1;
+fi
+
 CMD="curl -i -H \"Content-Type: application/json\" -X POST -d '{\"username\":\"$1\"}' http://localhost:5000/createPlayer"
 eval $CMD
 
