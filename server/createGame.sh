3fc0aeef9a1176c7edd1bf3edca2574f9131a3e2
rds89 <rds1989@hotmail.it>
2013-11-23 12:32:45 +0100

    Inserite eccezioni, snellito server.py e testato. Modificati script bash.

--- server/createGame.sh
+++ server/createGame.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 if test $# -ne 2; then  echo "Uso: $0 <username> <n_players>";  exit 1;
 fi
-CMD="curl -i -H \"Content-Type: application/json\" -X POST -d '{\"username\":\"$1\",\"n_players\":$2}' http://localhost:5000/createGame"
+CMD="curl -i -X POST http://localhost:5000/createGame/$1/$2"
 eval $CMD
