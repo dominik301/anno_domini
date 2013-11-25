57529408faafe9733c31db069878309ba8fab7b3
Vincenzo <gambale.vincenzo@gmail.com>
2013-11-25 19:48:33 +0100

    Ho cancellato nel server il while fatto per provare i "thread"

--- server/server.py
+++ server/server.py
@@ -30,10 +30,6 @@ def print_games():
 
 @app.route("/")
 def hello():
-	print "Sono in hello\n"
-	while True:
-		x=1
-	print "Fine hello\n"
 	return "sono il server di anno domini\n"
 
 @app.route('/createPlayer/<string:username>', methods = ['POST'])
