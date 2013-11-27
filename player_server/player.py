8f36ab30dad20c1a9ad20206a757e206ffd4470c
Stefano Bettinelli <stefano.bettinelli85@gmail.com>
2013-11-27 15:50:00 +0100

    Revert "Revert "dummy""
    This reverts commit f1a0463a0a7e6958be8ab5a3343c984d720f51c9.

--- player_server/player.py
+++ player_server/player.py
@@ -7,22 +7,19 @@ class Player:
 
 	"""A Player representation"""
 
-	def __init__(self, username=None, ip="", port=5000):
+	def __init__(self, username=None, ip=""):
 		if username == "" or ip == "":
 			raise ValueError("no username or missing ip")
 		else:
 			self.username = username
 			#request.remote_addr mi ritorna l'ip del client che richiede il servizio libreria flask
 			self.ip = ip
-			self.port = port
-
 	def __str__(self):
-		str = "Player_username: %s, ip: %s, port: %d" %(self.username, self.ip, self.port)
+		str = "Player_username: %s, ip: %s" %(self.username, self.ip)
 		return str
 
 	def to_json(self):
-		return jsonify(username = self.username, ip = self.ip, port = self.port)
+		return jsonify(username = self.username, ip = self.ip)
 
 if __name__ == "__main__":
-	a_player = Player("vincenzo", "0.0.0.0")
-	print a_player.username, " ", a_player.ip, " ", a_player.port
+	a_player = Player("vincenzo")
